a5ddf0227cf2a09f1fe006d5c418723a8af71f29
negronico <negdvp@gmail.com>
2023-10-18 11:37:35 -0300

    actualizacion

--- python/beginner/01.py
+++ python/beginner/01.py
@@ -7,3 +7,10 @@ def mezclador(string_a, string_b):
 
 mezclador("familia","abrigarse")
 
+#Escriba una función que reciba dos strings como parámetros y retorne un nuevo string que consista del primero, 
+#pero con el segundo string intercalado entre cada letra del primero.
+#Por ejemplo si los strings son "paz" y "so", entonces tu función debe retornar "psoasozso"
+def intercalar(string_a, string_b):
+  return string_a[0:1]+string_b+string_a[1:2]+string_b+string_a[-1]+string_b
+
+intercalar("PAZ","so")
